f007a456ec805ff8f1bd3b04f995da93bbccc7be
Wynston Hsu <wynstonhsu@Wynstons-MacBook-Pro.local>
2018-09-29 16:35:35 -0700

    ver. 1

---
+++ helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Wynston Hsu on 9/29/18.
+//  Copyright © 2018 Wynston Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
